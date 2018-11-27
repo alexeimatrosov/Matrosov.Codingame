2c8d6ff397b627c9474af1ca0f8f9fbd11e1034b
Alexei Matrosov <alexei@matrosov.by>
2018-11-27 14:01:26 +0200

    TeadsSponsoredContest F# solution

---
+++ Source/Laconic.Codingame/TeadsSponsoredContest.fs
@@ -0,0 +1,41 @@
+﻿module Laconic.Codigame.TeadsSponsoredContest
+
+open System
+
+let readString() = Console.ReadLine()
+let readInt = readString >> int
+let readPair() =
+    readString().Split([|' '|])
+    |> Array.map int
+    |> (fun a -> (a.[0], a.[1]))
+
+let edges = Array.init (readInt()) (fun _ -> readPair())
+
+let graph =
+    edges
+    |> Seq.append (edges |> Seq.map (fun (u,v) -> (v,u)))
+    |> Seq.groupBy fst
+    |> Seq.map (fun (u, p) -> (u, p |> Seq.map snd |> Set.ofSeq))
+    |> Map.ofSeq
+
+let mutable visited : Set<int> = Set.empty
+
+let rec visit node depth =
+    seq {
+        yield depth
+        for i in graph.[node] do
+            if not (visited.Contains i)
+            then 
+                visited <- visited.Add i
+                yield visit i (depth+1)
+    }
+    |> Seq.max
+
+let (KeyValue(leaf,_)) =
+    graph
+    |> Map.filter (fun _ v -> v.Count = 1)
+    |> Seq.head    
+
+let maxDepth = visit leaf 0
+
+printfn "%d" (maxDepth / 2 + maxDepth % 2)
