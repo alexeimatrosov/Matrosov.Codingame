fd934c441e78e98a84a35d394a4cb51d8d0a5afd
Alexei Matrosov <alexei@matrosov.by>
2018-12-22 13:05:11 +0200

    Refactoring HorseRacingDuals

---
+++ Source/Matrosov.Codingame/HorseRacingDuals.fs
@@ -0,0 +1,12 @@
+﻿module Matrosov.Codingame.HorseRacingDuals
+
+open System
+
+let readString() = Console.ReadLine();
+let readInt = readString >> int
+
+Seq.init (readInt()) (fun _ -> readInt())
+|> Seq.sort
+|> Seq.fold (fun (m, p) x -> abs (p - x) |> min m, x) (Int32.MaxValue, 0)
+|> fst
+|> printfn "%d"
