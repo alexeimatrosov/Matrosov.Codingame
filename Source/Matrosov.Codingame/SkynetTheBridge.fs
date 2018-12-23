46b49188534fffc8c33d647ba6703a97d0ac9948
Alexei Matrosov <alexei@matrosov.by>
2018-12-23 11:34:34 +0200

    SkynetTheBridge. Now tries to save as many bikes as possible. To get additional achievement

--- Source/Matrosov.Codingame/SkynetTheBridge.fs
+++ Source/Matrosov.Codingame/SkynetTheBridge.fs
@@ -11,8 +11,6 @@ let bikesToSurvive = readInt()
 let lanes = Array.init 4 (fun _ -> readString())
 let laneLength = lanes.[0].Length
 
-let actions = [|"SPEED"; "WAIT"; "JUMP"; "SLOW"; "UP"; "DOWN"|]
-
 let isHoleHit count x y =
     lanes.[y]
     |> Seq.skip (x + 1)
@@ -24,7 +22,9 @@ let moveUp speed (x, y, _) = (x + speed, y - 1, (isHoleHit (speed - 1) x y) && (
 let moveDown speed (x, y, _) = (x + speed, y + 1, (isHoleHit (speed - 1) x y) && (isHoleHit speed x (y + 1)))
 let jump speed (x, y, _) = (x + speed, y, (x + speed) >= laneLength || lanes.[y].[x + speed] = '.')
 
-let calculateTurns speed bikes =
+let actions = [|"SPEED"; "WAIT"; "JUMP"; "SLOW"; "UP"; "DOWN"|]
+
+let tryCalculateTurns speed bikes bikesToSurvive =
     let rec aux speed bikes turns =
         let act f =
             bikes
@@ -34,12 +34,12 @@ let calculateTurns speed bikes =
 
         let tryAction action =
             match action with
-            | "SPEED" -> aux (speed + 1) (move (speed + 1) |> act) (action :: turns)
-            | "WAIT" -> if speed > 0 then aux speed (move speed |> act) (action :: turns) else None
-            | "JUMP" -> if speed > 0 then aux speed (jump speed |> act) (action :: turns) else None
-            | "SLOW" -> if speed > 1 then aux (speed - 1) (move (speed - 1) |> act) (action :: turns) else None
-            | "UP" -> if speed > 0 && (bikes |> Seq.forall (fun (_, y, _) -> y > 0)) then aux speed (moveUp speed |> act) (action :: turns) else None
-            | "DOWN" -> if speed > 0 && (bikes |> Seq.forall (fun (_, y, _) -> y < 3)) then aux speed (moveDown speed |> act) (action :: turns) else None
+            | "SPEED" -> aux (speed + 1) (act <| move (speed + 1)) (action :: turns)
+            | "WAIT" -> if speed > 0 then aux speed (act <| move speed) (action :: turns) else None
+            | "JUMP" -> if speed > 0 then aux speed (act <| jump speed) (action :: turns) else None
+            | "SLOW" -> if speed > 1 then aux (speed - 1) (act <| move (speed - 1)) (action :: turns) else None
+            | "UP" -> if speed > 0 && (bikes |> Seq.forall (fun (_, y, _) -> y > 0)) then aux speed (act <| moveUp speed) (action :: turns) else None
+            | "DOWN" -> if speed > 0 && (bikes |> Seq.forall (fun (_, y, _) -> y < 3)) then aux speed (act <| moveDown speed) (action :: turns) else None
             | _ -> None
 
         if bikes |> Array.length < bikesToSurvive then None
@@ -47,12 +47,12 @@ let calculateTurns speed bikes =
         else actions |> Array.tryPick tryAction
 
     aux speed bikes []
-    |> Option.get
 
 let speed = readInt()
 let bikes = Array.init bikesCount (fun _ -> readBike())
-    
-calculateTurns speed bikes
+
+[min 4 bikesCount .. -1 .. bikesToSurvive]
+|> Seq.pick (tryCalculateTurns speed bikes)
 |> Seq.rev
 |> Seq.iter (printfn "%s")
 
